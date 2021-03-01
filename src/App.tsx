1a879114d1da42b01096b8aa07670f8eade16ee5
Gaurav Gupta <g1561998@gmail.com>
2021-03-01 23:35:32 +0530

    finishing up the nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Amazon Clone</h1>
+      <Header />
     </div>
   );
 }
