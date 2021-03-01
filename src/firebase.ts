c624476dd6bff3fe43dd4e29b2ee61a6946e1fca
Gaurav Gupta <g1561998@gmail.com>
2021-03-01 19:45:16 +0530

    init

---
+++ src/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcZfDptoTTgpxa7ajE9ZHhauTOhpROwUA",
+  authDomain: "clone-bab1f.firebaseapp.com",
+  projectId: "clone-bab1f",
+  storageBucket: "clone-bab1f.appspot.com",
+  messagingSenderId: "579499843864",
+  appId: "1:579499843864:web:be271408baaea84b6cbe6d",
+};
